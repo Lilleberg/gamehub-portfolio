a79e0dd2212e38f8b9368a03449f5dd5c8019176
Maria Lilleberg Holm <maria.lilleberg@gmail.com>
2021-10-05 11:59:33 +0200

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,6 +1,5 @@
 const menu = document.querySelector(".navbar");
 const hambIcon = document.querySelector("#hamburger-menu");
-//const cartIcon = document.querySelector("#cart-icon");
 
 hambIcon.onclick = function () {
   if (menu.style.display === "block") {
@@ -8,14 +7,4 @@ hambIcon.onclick = function () {
   } else {
     menu.style.display = "block";
   }
-}
-
-function buttonClick(allButtons, prodArr) {
-  allButtons.forEach(function (button) {
-    button.onclick = function (event) {
-      const addItems = products.find(item => item.id === event.target.dataset.product);
-      prodArr.push(addItems);
-      localStorage.setItem("cart", JSON.stringify(prodArr));
-    }
-  });
 }
